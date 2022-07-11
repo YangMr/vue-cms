a2d70263197c86cf042c1a23bbddc1a96a55db5f
cstp <759168442@qq.com>
2022-07-11 09:36:58 +0800

    feat(新功能):axios二次封装以及本地存储封装与跨域处理

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+/**
+ * @author YangLing
+ * @date 2022/7/11 09:29
+ */
+module.exports = {
+  publicPath: './',
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 8080,
+    https: false,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_CROSS_ORIGIN,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
+  }
+}
